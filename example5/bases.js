7662d17d89a2bdbd16c9aa0fa132aae7939a7474
Haroun Moatassime <haroun.moatassime@gmail.com>
2019-03-20 10:33:41 +0100

    tp js functions

--- example5/bases.js
+++ example5/bases.js
@@ -1,5 +1,14 @@
 function multiply(a, b){
+	if((typeof a != "number") || (typeof b != "number")) {
+		throwError("First two arguments must be numbers");
+	}
 	return a*b;
 }
-var x = multiply(2, 3);
+var x = multiply(10,  'formation', 4);
 console.log(x);
+
+function throwError(message){
+	throw new Error(message)
+}
+
+
