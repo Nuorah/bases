8c973dbd27a774f03406e7bc044b6fb8f70e42cf
Haroun Moatassime <haroun.moatassime@gmail.com>
2019-03-20 12:20:22 +0100

    fin tp bases js

---
+++ example10/bases.js
@@ -0,0 +1,14 @@
+(function(window){
+
+	var haroun = {};
+
+	haroun.contacts = function(){
+		return [
+				{nom: "moatassime", prenom: "othman"},
+				{nom: "moatassime", prenom: "safae"}
+				]
+	};
+
+	window.haroun = haroun;
+
+})(window);
